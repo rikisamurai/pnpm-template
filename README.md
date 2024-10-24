6469086781b4da33da74a17914704a566b7ba85c
riki <shanyulong.riki@bytedance.com>
2024-10-24 21:43:37 +0800

    feat: add simple-git-hooks

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 😈DOU+ NEXT
+
+## Development
+
+* node ≥ 20.18.0
+* pnpm ≥ 9.12.2
+
+### Setup
+```bash
+# install and setup
+pnpm run setup
+```
---
+++ commitlint.config.js
@@ -0,0 +1,9 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'header-max-length': [2, 'always', 120],
+    // @see https://commitlint.js.org/reference/rules.html#type-enum
+    'type-enum': [2, 'always', ['feat', 'fix', 'perf', 'refactor', 'test', 'docs', 'chore', 'style', 'revert', 'ci', 'build']],
+  },
+};
